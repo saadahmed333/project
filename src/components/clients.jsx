44e676a26c867452e8e51b59b54c63a551a2afc5
saadahmed333 <93013829+saadahmed333@users.noreply.github.com>
2022-12-12 16:57:08 +0500

    working on client component

---
+++ src/components/clients.jsx
@@ -0,0 +1,17 @@
+function Clients() {
+    return (
+        <section className="text-gray-600 body-font px-[20px] sm:px-[0px]">
+  <div className="container lg:px-[150px] py-24 mx-auto">
+    <div className="flex justify-between flex-col sm:flex-row w-full mb-12 ">
+      <h1 className="sm:text-[50px] lg:leading-[70px]  text-[35px] font-bold title-font lg:w-[600px] mb-4 text-gray-900">Our clients</h1>
+      <p className="w-[300px]">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum sunt libero iusto enim debitis aut nulla minus? Aliquam, fugit nostrum.</p>
+    </div>
+    <div className="flex w-full bg-slate-400 sm:flex-row flex-col  sm:space-x-4 sm:space-y-0 space-y-4 sm:px-0 items-end">
+      <h1>saad</h1>
+    </div>
+  </div>
+</section>
+    )
+}
+
+export default Clients
--- src/containers/index.js
+++ src/containers/index.js
@@ -4,6 +4,8 @@ import Hovercontext from "../components/hovercontext";
 import Slider from "../components/slider";
 import Idea from "../components/idea";
 import SimpleSlider from "../components/slider2";
+import Clients from "../components/clients";
+import sideImage from "../assets/sideimage.png"
 function Home() {
   return (
     <div>
@@ -15,6 +17,10 @@ function Home() {
       <Slider />
       <Idea />
       <SimpleSlider />
+      <div>
+            <img src={sideImage} alt="" />
+        </div>
+      <Clients />
     </div>
   );
 }
