033e55a76aea05b9bdfc2bd9dc47115fa1b71a9f
saadahmed333 <93013829+saadahmed333@users.noreply.github.com>
2022-12-12 14:04:06 +0500

    completed header and front page

---
+++ src/containers/index.js
@@ -0,0 +1,13 @@
+import Header from "../components/header";
+import First from "../components/front";
+function Home() {
+  return (
+    <div>
+        <div className="bg-[#FBFBFE]">
+      <Header />
+        </div>
+      <First />
+    </div>
+  );
+}
+export default Home;
